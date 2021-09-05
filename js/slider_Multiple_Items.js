6bc2e9deb84e468c44c612b36ff965eba7c77d80
SamuelGoulart <62961331+SamuelGoulart@users.noreply.github.com>
2021-09-04 22:28:26 -0300

    Apresentação, categorias, quem somos, localização e baixe o aplicativo terminados.

--- js/slider_Multiple_Items.js
+++ js/slider_Multiple_Items.js
@@ -1,15 +1,25 @@
 const prev  = document.querySelector('.prev');
 const next = document.querySelector('.next');
-
 const track = document.querySelector('.track');
 
+const prev1  = document.querySelector('.prev1');
+const next1 = document.querySelector('.next1');
+const track1 = document.querySelector('.track1');
+
 let carouselWidth = document.querySelector('.carousel_container').offsetWidth;
+
+let carouselWidth1 = document.querySelector('.carousel_container1').offsetWidth;
 
 window.addEventListener('resize', () => {
   carouselWidth = document.querySelector('.carousel_container').offsetWidth;
 })
 
+window.addEventListener('resize', () => {
+  carouselWidth1 = document.querySelector('.carousel_container1').offsetWidth;
+})
+
 let index = 0;
+let index1 = 0;
 
 next.addEventListener('click', () => {
   index++;
@@ -21,6 +31,16 @@ next.addEventListener('click', () => {
   }
 })
 
+next1.addEventListener('click', () => {
+  index1++;
+  prev1.classList.add('show');
+  track1.style.transform = `translateX(-${index1 * carouselWidth}px)`;
+  
+  if (track1.offsetWidth - (index1 * carouselWidth) < carouselWidth) {
+    next1.classList.add('hide');
+  }
+})
+
 prev.addEventListener('click', () => {
   index--;
   next.classList.remove('hide');
@@ -28,4 +48,16 @@ prev.addEventListener('click', () => {
     prev.classList.remove('show');
   }
   track.style.transform = `translateX(-${index * carouselWidth}px)`;
+})
+
+prev1.addEventListener('click', () => {
+  
+  index1--;
+  next1.classList.remove('hide');
+
+  if (index1 === 0) {
+    prev1.classList.remove('show');
+  }
+
+  track1.style.transform = `translateX(-${index1 * carouselWidth}px)`;
 })
